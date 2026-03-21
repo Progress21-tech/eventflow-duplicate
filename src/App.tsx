383a60601e5b7fdb4dc5fdce93a86145eed5d376
Progress Oni <dreaxellprobe@gmail.com>
2026-03-21 11:48:20 +0100

    Made certain changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-import ComingSoon from "./pages/ComingSoon";
 import Index from "./pages/Index";
 import Services from "./pages/Services";
 import About from "./pages/About";
@@ -27,8 +26,7 @@ const App = () => (
       <BrowserRouter>
         <AnimatePresence mode="wait">
           <Routes>
-            <Route path="/" element={<ComingSoon />} />
-            <Route path="/preview" element={<Index />} />
+            <Route path="/" element={<Index />} />
             <Route path="/services" element={<Services />} />
             <Route path="/about" element={<About />} />
             <Route path="/pricing" element={<Pricing />} />
@@ -36,8 +34,14 @@ const App = () => (
             <Route path="/v2" element={<V2 />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/case-study" element={<CaseStudy />} />
-            <Route path="/case-study/retire-young-rich" element={<CaseStudyRetireYoungRich />} />
-            <Route path="/case-study/youth-equip-bootcamp" element={<CaseStudyYouthEquip />} />
+            <Route
+              path="/case-study/retire-young-rich"
+              element={<CaseStudyRetireYoungRich />}
+            />
+            <Route
+              path="/case-study/youth-equip-bootcamp"
+              element={<CaseStudyYouthEquip />}
+            />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </AnimatePresence>
