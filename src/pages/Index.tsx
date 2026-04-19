73fee743ea411432bfacefa692589d7c704b536b
Progress Oni <dreaxellprobe@gmail.com>
2026-04-19 16:25:21 +0100

    Change logo image formats from PNG to JPG

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,9 +13,9 @@ import retireYoungImage from "@/assets/retire-young.jpg";
 import youthEquipImage from "@/assets/youth-equip.jpg";
 
 //Import Social Proofs Logos of Companies that have been worked with
-import logo1 from "@/assets/logo1.png";
-import logo2 from "@/assets/logo2.png";
-import logo3 from "@/assets/logo3.png";
+import logo1 from "@/assets/logo1.jpg";
+import logo2 from "@/assets/logo2.jpg";
+import logo3 from "@/assets/logo3.jpg";
 
 const logos = [logo1, logo2, logo3];
 
